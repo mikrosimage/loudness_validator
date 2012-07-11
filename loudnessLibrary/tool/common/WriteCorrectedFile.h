c02db89c5994a7c50c3ce8c4927d9500909624c2
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-07-11 16:52:42 +0200

    suppress correction into writing: only call functions

--- loudnessLibrary/tool/common/WriteCorrectedFile.h
+++ loudnessLibrary/tool/common/WriteCorrectedFile.h
@@ -1,22 +1,49 @@
+#ifndef WRITE_CORRECTED_FILE_H
+#define WRITE_CORRECTED_FILE_H
 
 #include <fstream>
+#include <vector>
 #include <SoundFile.h>
+#include <CorrectBuffer.h>
 #include <LookAheadLimiter.h>
-
-void writeCorrectedFile( SoundFile& input, SoundFile& output, const float gain, void (*callback)(int) )
+/*
+void correctBuffer( Loudness::LoudnessLibrary& analyser, float* data, const size_t samples, const size_t channelsInBuffer, const float gain )
 {
-	int bufferSize = input.rate () / 5;
-	int length = input.size();
-	size_t channelsInBuffer = input.chan();
-	size_t cumulOfSamples = 0;
+	float* dataPerChannel[ channelsInBuffer ];
 	
-	float* data [ channelsInBuffer ];
-	float* inpb  = new float [input.chan() * bufferSize];
+	for( size_t i = 0; i< channelsInBuffer; i++ )
+		dataPerChannel [i] = new float [samples];
+	
+	for( size_t i = 0; i < samples; i++ )
+	{
+		for( size_t c = 0; c < channelsInBuffer; c++ )
+		{
+			(*data) = dataPerChannel [c][i] = (*data) * gain;
+			data++;
+		}
+	}
+	analyser.processSamples( dataPerChannel, samples );
 	
 	for( size_t i=0; i < channelsInBuffer; i++ )
-		data [i] = new float [bufferSize];
+		delete[] dataPerChannel[i];
+}*/
+
+void writeCorrectedFile( Loudness::LoudnessLibrary& analyser, SoundFile& input, SoundFile& output, const float gain, void (*callback)(int) )
+{
+	int    bufferSize       = input.rate () / 5;
+	int    length           = input.size();
+	size_t channelsInBuffer = input.chan();
+	size_t cumulOfSamples   = 0;
+	
+	float* inpb             = new float [input.chan() * bufferSize];
+	float* dataPerChannel[ channelsInBuffer ];
+	
+	for( size_t i = 0; i< channelsInBuffer; i++ )
+		dataPerChannel [i] = new float [bufferSize];
 	
 	input.seek( 0 );
+	
+	analyser.initAndStart( channelsInBuffer, input.rate() );
 	
 	while (true)
 	{
@@ -24,65 +51,92 @@ void writeCorrectedFile( SoundFile& input, SoundFile& output, const float gain, 
 		if (samples == 0) break;
 		
 		float* ptr = inpb;
-		for( size_t i = 0; i < samples * channelsInBuffer; i++, ptr++ )
+		
+		correctBuffer( ptr, samples, channelsInBuffer, gain );
+		
+		// re-analyse output
+		ptr = inpb;
+		
+		for( int i = 0; i < samples; i++ )
 		{
-			*ptr = (*ptr) * gain;
+			for( size_t c = 0; c < channelsInBuffer; c++ )
+			{
+				dataPerChannel [c][i] = (*ptr);
+				ptr++;
+			}
 		}
+
+		analyser.processSamples( dataPerChannel, samples );
 		
 		samples = output.write( inpb, samples );
 		cumulOfSamples += samples;
 		callback( (float)cumulOfSamples / length * 100 );
 	}
-	
-	for( size_t i=0; i< channelsInBuffer; i++ )
-		delete[] data[i];
+	for( size_t i=0; i < channelsInBuffer; i++ )
+		delete[] dataPerChannel[i];
 	delete[] inpb;
 }
 
-void writeCorrectedFile( SoundFile& input, SoundFile& output, const float gain, const float attackMs, const float releaseMs, float lookAhead, float threshold, void (*callback)(int) )
+void writeCorrectedFile( Loudness::LoudnessLibrary& analyser, SoundFile& input, SoundFile& output, const float gain, float lookAhead, float threshold, void (*callback)(int) )
 {
-	int bufferSize = input.rate () / 5;
-	int length = input.size();
+	int    bufferSize       = input.rate () / 5;
+	int    length           = input.size();
 	size_t channelsInBuffer = input.chan();
-	size_t cumulOfSamples = 0;
-	
-	float* data [ channelsInBuffer ];
-	float* inpb  = new float [input.chan() * bufferSize];
-	
-	for( size_t i=0; i < channelsInBuffer; i++ )
-		data [i] = new float [bufferSize];
-	
-	input.seek( 0 );
+	size_t cumulOfSamples   = 0;
 
-	//Limiter limiter( attackMs, releaseMs, input.rate(), threshold, lookAhead );
+	float* inpb  = new float [ input.chan() * bufferSize ];
+	float* dataPerChannel[ channelsInBuffer ];
 	
-	float inputGain = 1.0;
-	float outputGain = 1.0;
+	for( size_t i = 0; i< channelsInBuffer; i++ )
+		dataPerChannel [i] = new float [bufferSize];
 	
-	LookAheadLimiter limiter( lookAhead, inputGain, outputGain, releaseMs, input.rate(), threshold );
+	std::vector<LookAheadLimiter*> limiters;
+	
+	for( size_t i = 0; i< channelsInBuffer; i++ )
+	{
+		LookAheadLimiter* lim = new LookAheadLimiter( lookAhead, input.rate(), threshold );
+		limiters.push_back( lim );
+	}
+
+	input.seek( 0 );
+	
+	analyser.initAndStart( channelsInBuffer, input.rate() );
 	
 	while (true)
 	{
 		int  samples = input.read( inpb, bufferSize );
-		if (samples == 0) break;
+		if( samples == 0 ) break;
 		
 		float* ptr = inpb;
-		for( size_t i = 0; i < samples * channelsInBuffer; i++, ptr++ )
+		
+		correctBuffer( limiters, ptr, samples, channelsInBuffer, gain );
+		
+		// re-analyse output
+		ptr = inpb;
+		
+		for( int i = 0; i < samples; i++ )
 		{
-			*ptr = limiter.process( (*ptr) * gain );
+			for( size_t c = 0; c < channelsInBuffer; c++ )
+			{
+				dataPerChannel [c][i] = (*ptr);
+				ptr++;
+			}
 		}
+
+		analyser.processSamples( dataPerChannel, samples );
 		
 		samples = output.write( inpb, samples );
+		
 		cumulOfSamples += samples;
 		callback( (float)cumulOfSamples / length * 100 );
 	}
 	
-	for( size_t i=0; i< channelsInBuffer; i++ )
-		delete[] data[i];
+	for( size_t i = 0; i< channelsInBuffer; i++ )
+	{
+		delete limiters.at( i );
+		delete[] dataPerChannel[i];
+	}
 	delete[] inpb;
 }
 
-
-//std::pow ( 10, ( correctionGain ) / 20 )
-
-
+#endif
