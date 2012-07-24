77989508b85b83b57f1d437a8a67481903d0b637
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-07-24 16:12:53 +0200

    adding mising file: RollingMax.hpp

---
+++ loudnessLibrary/tool/common/RollingMax.hpp
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////////////////
+// RollingMax.hpp
+//
+// Marc-Antoine ARNAUD 2012
+
+#ifndef ROLLING_MAX_HPP
+#define ROLLING_MAX_HPP
+
+#include <boost/circular_buffer.hpp>
+
+template<typename Sample>
+class RollingMax
+{
+public:
+	RollingMax( size_t windowSize );
+	
+	void operator()(Sample sample);
+
+	Sample getMax();
+	
+private:
+	boost::circular_buffer<Sample> cb;
+	Sample max;
+};
+
+template<typename Sample>
+RollingMax<Sample>::RollingMax( size_t windowSize ) :
+	cb( windowSize )
+{
+}
+
+template<typename Sample>
+void RollingMax<Sample>::operator()(Sample sample)
+{
+	
+	Sample removedSample = cb.front();
+	
+	cb.push_back( sample );
+	
+	if( removedSample == max && sample < max )
+	{
+		max = 0;
+		for( size_t i = 0; i < cb.size(); i++ )
+		{
+			boost::numeric::max_assign( max, cb.at(i));
+		}
+	}
+	boost::numeric::max_assign( max, sample );
+}
+
+template<typename Sample>
+Sample RollingMax<Sample>::getMax()
+{
+	return max;
+}
+
+#endif
