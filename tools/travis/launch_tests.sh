9f96b46856bc4b8ae7e67174e2f7b574acc5c5d1
Clement Champetier <cnt@mikrosimage.eu>
2016-03-24 12:00:32 +0100

    scons: fixed how to run unit tests
    * Removed test alias within scons (it is not working in all tested
    environments, ie Travis).
    * Added shell script to manually run the unit tests.

---
+++ tools/travis/launch_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+# Print commands and their arguments as they are executed.
+set -x
+
+# Run tests
+./build/release/test/test
