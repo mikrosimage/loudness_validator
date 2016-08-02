8ee5b24b1da94ae0bf3073447e0a28111b498ad3
Clement Champetier <cnt@mikrosimage.eu>
2016-08-02 14:07:07 +0200

    Travis: added build of mediaAnalyser app
    * Install avtranscoder (with ffmpeg).
    * Build the mediaAnalyser application.

--- tools/travis/install_dependencies.sh
+++ tools/travis/install_dependencies.sh
@@ -12,6 +12,12 @@ cmake . -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL}
 make -k
 make install
 
+# Install avtranscoder
+ARCHIVE_FILE=avtranscoder-linux-gcc-ffmpeg-${FFMPEG_VERSION}.tgz
+wget https://github.com/avTranscoder/avTranscoder/releases/download/v${AVTRANSCODER_VERSION}/${ARCHIVE_FILE}
+tar xzf ${ARCHIVE_FILE}
+rm ${ARCHIVE_FILE}
+
 # Install additional dependencies in case of osx environment
 if [ ${TRAVIS_OS_NAME} = "osx" ]; then
     brew update
