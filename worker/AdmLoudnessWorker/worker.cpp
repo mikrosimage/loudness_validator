687e4f4df2d17326ab2dd09fefa211dabe931cde
Valentin NOEL <valentin.noel@media-io.com>
2019-12-18 16:03:52 +0100

    Update AdmLoudnessAnalyser and AdmLoudnessWorker applications, after AdmLoudnessAnalyser library refactoring

--- worker/AdmLoudnessWorker/worker.cpp
+++ worker/AdmLoudnessWorker/worker.cpp
@@ -7,48 +7,92 @@
 
 #include <admLoudnessAnalyser/AdmLoudnessAnalyser.hpp>
 
+std::map<std::string, float> parseElementGains(const std::string& elementGainsStr) {
+  std::map<std::string, float> elementGains;
+  const size_t arrayInPos = elementGainsStr.find("[");
+  if(arrayInPos ==  std::string::npos) {
+    throw std::runtime_error("Invalid gains mapping string format: missing '[' as first character.");
+  }
+  const size_t arrayOutPos = elementGainsStr.find("]");
+  if(arrayOutPos ==  std::string::npos) {
+    throw std::runtime_error("Invalid gains mapping string format:  missing ']' as last character.");
+  }
+  const std::string gainPairsStr = elementGainsStr.substr(arrayInPos + 1, arrayOutPos - 1);
+
+  size_t nextPosition = 0;
+  std::string separator = ", ";
+  while(nextPosition < gainPairsStr.size()){
+    size_t separatorPos = gainPairsStr.find(separator, nextPosition);
+
+    if(separatorPos == std::string::npos){
+      separatorPos = gainPairsStr.size();
+    }
+
+    const std::string gainPair = gainPairsStr.substr(nextPosition + 1, separatorPos - (nextPosition + 2)); // "element_id=gain"
+    const size_t equalPos = gainPair.find("=");
+    const std::string elemId = gainPair.substr(0, equalPos); // "element_id"
+    const std::string gainDbStr = gainPair.substr(equalPos + 1, gainPair.size()); // "gain"
+    elementGains[elemId] = pow(10.0, std::atof(gainDbStr.c_str()) / 20.0);
+    std::cout << "Gain:                  " << elementGains[elemId] << " (" << gainDbStr << " dB) applied to " << elemId << std::endl;
+
+    nextPosition = separatorPos + separator.size();
+  }
+
+  return elementGains;
+}
+
 void displayUsage() {
   std::cout << "Parameters:      TYPE                           DESCRIPTION" << std::endl;
   std::cout << std::endl;
   std::cout << "  input          (string)                       BW64/ADM audio file path" << std::endl;
-  std::cout << "  output         (string) (optional)            Destination directory" << std::endl;
+  std::cout << "  output         (string) (optional)            Destination file (analyse) / directory (correction)" << std::endl;
   std::cout << "  display        (boolean) (optional)           Display loudness analyse values" << std::endl;
-  std::cout << "  correction     (boolean) (optional)           Enable correction (if an output file is specified)" << std::endl;
+  std::cout << "  correction     (boolean) (optional)           Enable correction (if an output directory is specified)" << std::endl;
   std::cout << "  limiter        (boolean) (optional)           Enable peak limiter (if correction is enabled)" << std::endl;
   std::cout << "  element_id     (string) (optional)            Select the AudioProgramme to be analysed by `element_id`" << std::endl;
+  std::cout << "  gain_mapping   (array_of_strings) (optional)  Array of `ELEMENT_ID=GAIN` strings, where `GAIN` is the gain value (in dB) to apply to ADM element defined by its `ELEMENT_ID`" << std::endl;
   std::cout << std::endl;
 }
 
 int analyse(const std::string& inputFilePath,
-            const char* outputFilePathStr,
+            const char* outputPathStr,
             const char* elementIdToRenderStr,
+            const char* elementGainsCStr,
             const bool displayValues = false,
             const bool enableCorrection = false,
             const bool enableLimiter = false) {
 
-  const std::string outputFilePath = (outputFilePathStr)? outputFilePathStr : "";
+  const std::string outputPath = (outputPathStr)? outputPathStr : "";
   const std::string elementIdToRender = (elementIdToRenderStr)? elementIdToRenderStr : "";
 
-  if(outputFilePath.empty() && (enableCorrection || enableLimiter)) {
-      std::cerr << "Error: an output file must be specified to enable correction and limiter." << std::endl;
-      displayUsage();
-      return 1;
+  if(outputPath.empty() && enableCorrection) {
+    const std::string errorMessage = "An output file must be specified to enable correction.";
+    std::cerr << "Error: " << errorMessage << std::endl;
+    displayUsage();
+    return 1;
   }
   if(enableLimiter && !enableCorrection) {
-      std::cerr << "Error: correction must be enabled to enable limiter." << std::endl;
-      displayUsage();
-      return 1;
+    const std::string errorMessage = "Correction must be enabled to enable limiter.";
+    std::cerr << "Error: " << errorMessage << std::endl;
+    displayUsage();
+    return 1;
   }
 
   const std::string outputLayout("0+2+0");
+  std::map<std::string, float> elementGains;
+  if(elementGainsCStr) {
+    elementGains = parseElementGains(elementGainsCStr);
+  }
 
   std::cout << "Input file:            " << inputFilePath << std::endl;
   std::cout << "Output layout:         " << outputLayout << std::endl;
-  std::cout << "Output file:           " << outputFilePath << std::endl;
-  std::cout << "ADM element to render: " << elementIdToRender << std::endl;
-  std::cout << "Display result:        " << displayValues << std::endl;
-  std::cout << "Correction enabled:    " << enableCorrection << std::endl;
-  std::cout << "Peak limiter enabled:  " << enableLimiter << std::endl;
+  std::cout << "Output path:           " << (outputPath.empty()? "-" : outputPath) << std::endl;
+  std::cout << "ADM element to render: " << (elementIdToRender.empty()? "-" : elementIdToRender) << std::endl;
+  std::cout << "Display result:        " << (displayValues? "true" : "false") << std::endl;
+  std::cout << "Correction enabled:    " << (enableCorrection? "true" : "false") << std::endl;
+  if(enableCorrection) {
+    std::cout << "Peak limiter enabled:  " << (enableLimiter? "true" : "false") << std::endl;
+  }
 
   try {
       Loudness::admanalyser::AdmLoudnessAnalyser analyser(inputFilePath, outputLayout, outputFilePath, elementIdToRender);
@@ -191,19 +235,19 @@ int process(JobParameters job, GetParameterValueCallback parametersValueGetter, 
     	displayUsage();
     	return 1;
     }
-    logger(inputFilePath);
 
     char* outputFilePath = parametersValueGetter(job, "output");
     char* elementIdToRender = parametersValueGetter(job, "element_id");
     char* displayValuesStr = parametersValueGetter(job, "display");
     char* enableCorrectionStr = parametersValueGetter(job, "correction");
     char* enableLimiterStr = parametersValueGetter(job, "limiter");
+    char* elementGainsCStr = parametersValueGetter(job, "gain_mapping");
 
     bool displayValues = displayValuesStr && strcmp(displayValuesStr, "true") == 0;
     bool enableCorrection = enableCorrectionStr && strcmp(enableCorrectionStr,"true") == 0;
     bool enableLimiter = enableLimiterStr && strcmp(enableLimiterStr,"true") == 0;
 
-    return analyse(inputFilePath, outputFilePath, elementIdToRender, displayValues, enableCorrection, enableLimiter);
+    return analyse(inputFilePath, outputFilePath, elementIdToRender, elementGainsCStr, displayValues, enableCorrection, enableLimiter);
 }
 
 #ifdef __cplusplus
