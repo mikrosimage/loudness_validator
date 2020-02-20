850938bc8a2246065a39a446a4acf75f097b1896
Valentin NOEL <valentin.noel@media-io.com>
2020-02-20 10:48:10 +0100

    Travis: force G++ version on MacOSx

--- tools/travis/install_dependencies.sh
+++ tools/travis/install_dependencies.sh
@@ -5,9 +5,15 @@ set -e
 # Print commands and their arguments as they are executed.
 set -x
 
+# Install additional dependencies in case of osx environment
+if [ ${TRAVIS_OS_NAME} = "osx" ]; then
+    brew update
+    brew install scons lcov libsndfile qt gcc@7
+fi
+
 # Install GTest framework
 if [[ ! -d "${GTEST_INSTALL}/*" ]]; then
-    if [ "$TRAVIS_COMPILER" == "gcc" ]; then
+    if [ "$TRAVIS_OS_NAME" == "linux" ] && [ "$TRAVIS_COMPILER" == "gcc" ]; then
         export CC=/usr/bin/gcc-7
         export CXX=/usr/bin/g++-7
     fi
@@ -24,10 +30,4 @@ if [[ ! -d "${AVTRANSCODER_INSTALL}/*" ]]; then
     wget https://github.com/avTranscoder/avTranscoder/releases/download/v${AVTRANSCODER_VERSION}/${ARCHIVE_FILE}
     tar xzf ${ARCHIVE_FILE} -C ${TRAVIS_BUILD_DIR}
     rm ${ARCHIVE_FILE}
-fi
-
-# Install additional dependencies in case of osx environment
-if [ ${TRAVIS_OS_NAME} = "osx" ]; then
-    brew update
-    brew install scons lcov libsndfile qt
 fi
