434a7241c4580e93a8fa8473c8327522103cf1fa
Valentin NOEL <valentin.noel@media-io.com>
2022-04-05 12:34:15 +0200

    Add CMake media-analyser app build definition

---
+++ cmake/FindAvTranscoder.cmake
@@ -0,0 +1,19 @@
+# - Find AvTranscoder library
+# Find the native AvTranscoder includes and library.
+# 
+# This module defines:
+# AVTRANSCODER_FOUND
+# AVTRANSCODER_LIBRARIES - The AvTranscoder libraries.
+#
+
+find_library(AVTRANSCODER_LIBRARY avtranscoder)
+find_path(AVTRANSCODER_INCLUDE_DIR AvTranscoder/common.hpp)
+
+# Get AvTranscoder from custom install
+if(AVTRANSCODER_LIBRARY AND AVTRANSCODER_INCLUDE_DIR)
+	set(AVTRANSCODER_LIBRARIES "${AVTRANSCODER_LIBRARY}")
+	message("-- Found AvTranscoder: " ${AVTRANSCODER_LIBRARY})
+	set(AVTRANSCODER_FOUND TRUE)
+else()
+	set(AVTRANSCODER_FOUND FALSE)
+endif()
